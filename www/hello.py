20faebb9d26b43de37b7b9ab5af779300cf09c6d
zhangbbb <602437897@qq.com>
2017-12-02 22:46:31 +0800

    render_template

--- www/hello.py
+++ www/hello.py
@@ -1,34 +1,16 @@
 from flask import Flask
-from flask import make_response
-from flask import abort
+from flask import render_template
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    return '<h1>Hello World!</h1>'
+    return render_template('index.html')
 
 
 @app.route('/test/<name>')
 def test(name):
-    return '<h1>Hello {}!</h1>'.format(name)
-
-
-@app.route('/test1')
-def test1():
-    return 'Bad Request',400
-
-
-@app.route('/cookie')
-def cookies():
-    response = make_response('<h1>this document is carried by cookie</h1>')
-    response.set_cookie('answer', 'this document is carried by cookie')
-    return response
-
-
-@app.route('/test_abort')
-def test_abort():
-    abort(404)
+    return render_template('test.html', name=name)
 
 
 if __name__ == '__main__':
