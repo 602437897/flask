5dc195465bffcf679fd87899c90d60b7471ec070
zhangbbb <602437897@qq.com>
2017-12-03 19:31:18 +0800

    moment

--- www/hello.py
+++ www/hello.py
@@ -1,25 +1,33 @@
-from flask import Flask
-from flask import render_template
+from datetime import datetime
+from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
+from flask_moment import Moment
+
 app = Flask(__name__)
 
-
 bootstrap = Bootstrap(app)
-
-
-@app.route('/')
-def index():
-    return render_template('index.html')
-
-
-@app.route('/user/<name>')
-def user(name):
-    return render_template('user.html', name=name)
+moment = Moment(app)
 
 
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template('404.html'), 404
+
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'), 500
+
+
+@app.route('/')
+def index():
+    return render_template('index.html',
+                           current_time=datetime.utcnow())
+
+
+@app.route('/user/<name>')
+def user(name):
+    return render_template('user.html', name=name)
 
 
 if __name__ == '__main__':
